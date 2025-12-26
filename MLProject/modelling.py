a9a65b012dddcfec9452cc59c057dae0914112e1
azka-yazid <azkay1603@gmail.com>
2025-12-26 12:18:59 +0700

    Kriteria 3

---
+++ MLProject/modelling.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+mlflow.set_experiment("MSML-Glass-CI")
+
+df = pd.read_csv("glass_clean.csv")
+X = df.drop(columns=["Type"])
+y = df["Type"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+model = Pipeline([
+    ("scaler", StandardScaler()),
+    ("clf", LogisticRegression(max_iter=3000))
+])
+
+mlflow.sklearn.autolog(log_models=True)
+
+with mlflow.start_run(run_name="logreg_autolog"):
+    joblib.dump(X.columns.tolist(), "feature_columns.pkl")
+    mlflow.log_artifact("feature_columns.pkl")
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("test_accuracy_manual", acc)
+
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure()
+    plt.imshow(cm)
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.tight_layout()
+    plt.savefig("confusion_matrix.png")
+    plt.close()
+    mlflow.log_artifact("confusion_matrix.png")
